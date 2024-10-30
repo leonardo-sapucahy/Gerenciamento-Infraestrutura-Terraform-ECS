2c024c300ac00b2e3e4514c3ac8b0b5cefd38592
Leonardo Sapucahy <leosapucahy19@gmail.com>
2024-10-30 14:14:44 -0300

    Create ecr.tf

---
+++ terraform/ECS/ecr.tf
@@ -0,0 +1,35 @@
+resource "aws_ecr_repository" "repository_coloque aqui o nome do repositorio" {
+  name = "coloque aqui o nome do repositorio"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
+
+resource "aws_ecr_repository" "repository_coloque aqui o nome do repositorio" {
+  name = "coloque aqui o nome do repositorio"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
+
+resource "aws_ecr_repository" "repository_coloque aqui o nome do repositorio" {
+  name = "coloque aqui o nome do repositorio"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
+
+resource "aws_ecr_repository" "repository_coloque aqui o nome do repositorio" {
+  name = "coloque aqui o nome do repositorio"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
